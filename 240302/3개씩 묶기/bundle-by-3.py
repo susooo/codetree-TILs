384ce355a4fdd1bf453c05bb6a81ffeb24cf436b
김수현 <92291198+susooo@users.noreply.github.com>
2024-03-02 14:27:51 +0900

    Title: 3개씩 묶기, Time: 123ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240302/3개씩 묶기/bundle-by-3.py
+++ 240302/3개씩 묶기/bundle-by-3.py
@@ -2,11 +2,7 @@ n = int(input())
 num_list = [x for x in map(int, input().split())]
 num_list.sort()
 sum = sum(num_list)
-
-if n%3 == 0:
-    count = n//3
-else:
-    count = n//3+1
+count = n//3
 
 for i in range(count):
     sum -= num_list[i]
