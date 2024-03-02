2cfe13a38b06812cde0e2796c11c48b5031ef418
김수현 <92291198+susooo@users.noreply.github.com>
2024-03-02 20:35:07 +0900

    Title: 연속된 숫자 만들기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/연속된 숫자 만들기/create-consecutive-numbers.py
+++ 240302/연속된 숫자 만들기/create-consecutive-numbers.py
@@ -2,19 +2,13 @@ people = [x for x in map(int, input().split())]
 
 if people[1]-people[0]==1 and people[2]-people[1]==1:
     min_count = 0
-elif people[1]-people[0]==1:
-    min_count = (people[2]-people[1])//2
-elif people[2]-people[1]==1:
-    min_count = (people[1]-people[0]-1)//2
+elif people[1]-people[0]==2 or people[2]-people[1]==2:
+    min_count = 1
 else:
-    min_count = min(people[1]-people[0],people[2]-people[1])-1
+    min_count = 2
 
 if people[1]-people[0]==1 and people[2]-people[1]==1:
     max_count = 0
-elif people[1]-people[0]==1:
-    max_count = people[2]-people[1]-1
-elif people[2]-people[1]==1:
-    max_count = people[1]-people[0]-1
 else:
     max_count = max(people[1]-people[0],people[2]-people[1])-1
 print(min_count)
