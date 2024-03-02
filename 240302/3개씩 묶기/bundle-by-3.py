77ebf8719505ca76387844a859d2f7fb2b1f4601
김수현 <92291198+susooo@users.noreply.github.com>
2024-03-02 14:51:20 +0900

    Title: 3개씩 묶기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240302/3개씩 묶기/bundle-by-3.py
+++ 240302/3개씩 묶기/bundle-by-3.py
@@ -1,10 +1,11 @@
 n = int(input())
 num_list = [x for x in map(int, input().split())]
-num_list.sort()
+num_list.sort(reverse=True)
 sum = sum(num_list)
 
 for i in range(0,len(num_list),3):
-    sum -= num_list[i]
+    if num_list[i+2]:
+        sum -= num_list[i+2]
     if i+3 > len(num_list):
         break
 print(sum)
