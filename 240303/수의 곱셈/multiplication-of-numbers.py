1b89aaf25bfdb523e9acb58d95982d8c3133f262
김수현 <92291198+susooo@users.noreply.github.com>
2024-03-03 23:49:26 +0900

    Title: 수의 곱셈, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/수의 곱셈/multiplication-of-numbers.py
+++ 240303/수의 곱셈/multiplication-of-numbers.py
@@ -1,9 +1,12 @@
 n_list = [x for x in map(int, input().split())]
 n_list.sort()
 answer = 1
+count = 0
+
 for num in n_list:
     if num%2 != 0:
         answer*=num
-if answer == 1:
+        count+=1
+if count == 0:
     answer = n_list[0]*n_list[1]*n_list[2]
 print(answer)
