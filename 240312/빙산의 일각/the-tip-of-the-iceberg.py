cd5c4709791a396d7d35eace673a04b9db627ead
김수현 <92291198+susooo@users.noreply.github.com>
2024-03-12 12:13:08 +0900

    Title: 빙산의 일각, Time: 5091ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240312/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240312/빙산의 일각/the-tip-of-the-iceberg.py
@@ -4,14 +4,14 @@ count = 0
 for i in range(n):
     h = int(input())
     num.append(h)
-max_h = max(num)
-min_h = min(num)
+min_h, max_h = min(num), max(num)
+
 for i in range(min_h,max_h):
     tmp = 0
-    for j in range(n):
-        if num[j]-i == 0:
+    for j in range(n-1):
+        if num[j]-i > 0 and num[j+1]-i <= 0:
             tmp += 1
-    if num[n-1] != 0:
+    if num[n-1]-i > 0:
         tmp += 1
     if tmp > count:
         count = tmp
