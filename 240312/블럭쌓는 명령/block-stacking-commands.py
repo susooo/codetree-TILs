2430768d68bdb6dbe6df2364d4f620026ab2dd55
김수현 <92291198+susooo@users.noreply.github.com>
2024-03-12 09:52:38 +0900

    Title: 블럭쌓는 명령, Time: 5096ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

---
+++ 240312/블럭쌓는 명령/block-stacking-commands.py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+n_list = [0]*(n+1)
+for i in range(k):
+    a,b = map(int, input().split())
+    for j in range(a, b+1):
+        n_list[j] += 1
+n_list.sort()
+x = n//2+1
+print(n_list[x])
