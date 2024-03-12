6677b5181e1a3ea6164d70975fea259096901e06
김수현 <92291198+susooo@users.noreply.github.com>
2024-03-12 10:35:39 +0900

    Title: 블럭쌓는 명령, Time: 230ms, Memory: 39MB, Status: Passed - CodeTree

--- 240312/블럭쌓는 명령/block-stacking-commands.py
+++ 240312/블럭쌓는 명령/block-stacking-commands.py
@@ -1,11 +1,17 @@
-import sys
-input = sys.stdin.readline
 n, k = map(int, input().split())
 n_list = [0]*(n+1)
-for i in range(k):
-    a,b = map(int, input().split())
-    for j in range(a, b+1):
-        n_list[j] += 1
+
+for _ in range(k):
+    a, b = map(int, input().split())
+    n_list[a] += 1
+    if b + 1 <= n:
+        n_list[b + 1] -= 1
+
+prefix_sum = 0
+for i in range(1, n + 1):
+    prefix_sum += n_list[i]
+    n_list[i] = prefix_sum
+
 n_list.sort()
-x = n//2+1
+x = n // 2 + 1
 print(n_list[x])
