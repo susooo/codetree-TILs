d973545901dbd979a37c55c07bcefc9477b0a4b4
김수현 <92291198+susooo@users.noreply.github.com>
2024-10-11 15:29:22 +0900

    Title: 생명과학부 랩 인턴, Time: 2208ms, Memory: 30MB, Status: Passed - CodeTree

--- 241011/생명과학부 랩 인턴/biology-lab-intern.py
+++ 241011/생명과학부 랩 인턴/biology-lab-intern.py
@@ -1,15 +1,10 @@
 from collections import deque
 
-n,m,k = map(int,input().split())
-direction = {1:(-1,0), 2:(1,0), 3:(0,1), 4:(0,-1)}
-grid = [[0 for _ in range(m)] for _ in range(n)]
-germ = deque()
-
 def change_direction(x,y,d):
-    if x==0 and d==1: d=2
-    elif x==n-1 and d==2: d=1
-    elif y==0 and d==4: d=3
-    elif y==m-1 and d==3: d=4
+    if d==1 and x==0: d=2
+    elif d==2 and x==n-1: d=1
+    elif d==4 and y==0: d=3
+    elif d==3 and y==m-1: d=4
 
     return x,y,d
     
@@ -25,43 +20,49 @@ def move_germ():
 
     while germ:
         x,y,s,d,b = germ.popleft()
-        grid[x][y] = 0
         for _ in range(s):
-            x,y,d = change_direction(x,y,d)
+            nx,ny,d = change_direction(x,y,d)
             dx,dy = direction[d]
-            x,y = x+dx, y+dy
+            x,y = nx+dx, ny+dy
 
         if not grid[x][y]:
             grid[x][y] = (x,y,s,d,b)
         else:
-            x2,y2,s2,d2,b2 = grid[x][y]
-            grid[x][y] = compare((x,y,s,d,b), (x2,y2,s2,d2,b2))
+            grid[x][y] = compare((x,y,s,d,b), grid[x][y])
     
     for i in range(n):
         for j in range(m):
             if grid[i][j]:
                 germ.append(grid[i][j])
+                grid[i][j] = 0
 
 def intern(j):
     global total
     x,y,s,d,b = -1,-1,-1,-1,-1
     for i in range(n):
-        if grid[i][j]:
-            x,y,s,d,b = grid[i][j]
-            grid[i][j] = 0
+        if grid_germ[i][j]:
+            x,y,s,d,b = grid_germ[i][j]
+            grid_germ[i][j] = 0
             total += b
             break
     return x,y,s,d,b
 
+n,m,k = map(int,input().split())
+direction = {1:(-1,0), 2:(1,0), 3:(0,1), 4:(0,-1)}
+grid = [[0 for _ in range(m)] for _ in range(n)]
+germ = deque()
 total = 0
+
 for _ in range(k):
     x,y,s,d,b = map(int, input().split())
-    x-=1
-    y-=1
-    grid[x][y] = (x,y,s,d,b)
+    x,y = x-1,y-1
     germ.append((x,y,s,d,b))
 
 for j in range(m):
+    grid_germ = [[0 for _ in range(m)] for _ in range(n)]
+    for tx,ty,ts,td,tb in germ:
+        grid_germ[tx][ty] =  (tx,ty,ts,td,tb)
+
     rx,ry,rs,rd,rb = intern(j)
     if rx != -1:
         germ.remove((rx,ry,rs,rd,rb))
